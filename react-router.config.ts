c8c0d786eb5a3f4f1eae4291e080f06c78953d38
Pete Bacon Darwin <pbacondarwin@cloudflare.com>
2026-01-07 14:07:44 +0000

    fix builds

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
-  buildDirectory: "build",
+  future: {
+    unstable_viteEnvironmentApi: true,
+  },
 } satisfies Config;
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,8 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    outDir: "build",
-    emptyOutDir: true,
-  },
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
-    reactRouter({
-      buildDirectory: "build",
-    }),
+    reactRouter(),
   ],
 });
